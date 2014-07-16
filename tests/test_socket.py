6508fec724bed3b8af3d32465c6309b9a44955e2
Noah Goldman <noahg34@gmail.com>
2014-07-16 01:25:40 -0400

    Updated tests for pytest

--- tests/test_socket.py
+++ tests/test_socket.py
@@ -1,96 +1,92 @@
 from unittest import TestCase
+import pytest
 
 from adbpy.socket import Socket, int_to_hex, make_adb_message
 from tests.mock_socket import MockSocket
 
-class TestAdbCommand(TestCase):
+def test_int_to_hex():
+    assert int_to_hex(2) == "0002"
+    assert int_to_hex(255) == "00ff"
+    assert int_to_hex(256) == "0100"
+    assert int_to_hex(100) == "0064"
+    assert int_to_hex(0) == "0000"
 
-    def test_int_to_hex(self):
-        self.assertEqual(int_to_hex(2), "0002")
-        self.assertEqual(int_to_hex(255), "00ff")
-        self.assertEqual(int_to_hex(256), "0100")
-        self.assertEqual(int_to_hex(100), "0064")
-        self.assertEqual(int_to_hex(0), "0000")
+def test_int_to_hex_overflow():
+    with pytest.raises(ValueError):
+        int_to_hex(65536)
 
-    def test_int_to_hex_overflow(self):
-        with self.assertRaises(ValueError):
-            int_to_hex(65536)
+def test_message():
+    msg = make_adb_message("host:track-devices")
+    assert msg == "0012host:track-devices"
 
-    def test_message(self):
-        msg = make_adb_message("host:track-devices")
-        self.assertEqual(msg, "0012host:track-devices")
+    msg = make_adb_message("host:serial:950a8ad5:list-forward")
+    assert msg == "0021host:serial:950a8ad5:list-forward"
 
-        msg = make_adb_message("host:serial:950a8ad5:list-forward")
-        self.assertEqual(msg, "0021host:serial:950a8ad5:list-forward")
-    
-    def test_message_overflow(self):
-        with self.assertRaises(ValueError):
-            make_adb_message("x" * 65536)
+def test_message_overflow():
+    with pytest.raises(ValueError):
+        make_adb_message("x" * 65536)
 
+@pytest.fixture
+def socket():
+    return Socket(MockSocket())
 
-class TestSocket(TestCase):
+def test_send_normal_response(socket):
+    data_sent = [] 
+    def send(message):
+        data_sent.append(message)
+        return len(message)
 
-    def setUp(self):
-        self.sock = MockSocket()
-        self.mysock = Socket(self.sock)
+    socket.socket.send = send
 
-    def test_send_normal_response(self):
-        data_sent = [] 
-        def send(message):
-            data_sent.append(message)
-            return len(message)
+    data = "host:track-devices"
 
-        self.sock.send = send
+    socket.send(data)
 
-        data = "host:track-devices"
+    assert len(data_sent) == 1
+    assert data == data_sent[0]
 
-        self.mysock.send(data)
+def test_send_staggered_response(socket):
+    data_sent = [] 
+    def send(message):
+        data_sent.append(message[0])
+        return 1
 
-        self.assertEquals(1, len(data_sent))
-        self.assertEquals(data, data_sent[0])
+    socket.socket.send = send
+    data = "host:track-devices"
 
-    def test_send_staggered_response(self):
-        data_sent = [] 
-        def send(message):
-            data_sent.append(message[0])
-            return 1
+    socket.send(data)
 
-        self.sock.send = send
-        data = "host:track-devices"
+    assert len(data) == len(data_sent)
+    assert data == ''.join(data_sent)
 
-        self.mysock.send(data)
+def test_send_failed_response(socket):
+    response_lengths = [0, 1]
+    socket.socket.send = lambda x: response_lengths.pop()
 
-        self.assertEquals(len(data), len(data_sent))
-        self.assertEquals(data, ''.join(data_sent))
+    with pytest.raises(RuntimeError):
+        socket.send("long test string")
 
-    def test_send_failed_response(self):
-        response_lengths = [0, 1]
-        self.sock.send = lambda x: response_lengths.pop()
+def test_receive_fixed_length_full_response(socket):
+    data_to_recv = "0005"
 
-        with self.assertRaises(RuntimeError):
-            self.mysock.send("long test string")
+    socket.socket.recv = lambda x: data_to_recv
 
-    def test_receive_fixed_length_full_response(self):
-        data_to_recv = "0005"
+    data = socket.receive_fixed_length(4)
+    assert data_to_recv == data
 
-        self.sock.recv = lambda x: data_to_recv
+def test_receive_fixed_length_staggered_response(socket):
+    data_to_recv = "0005"
+    split_data = list(data_to_recv)
+    split_data.reverse()
 
-        data =  self.mysock.receive_fixed_length(4)
-        self.assertEqual(data_to_recv, data)
+    socket.socket.recv = lambda x: split_data.pop()
 
-    def test_receive_fixed_length_staggered_response(self):
-        data_to_recv = "0005"
-        split_data = list(data_to_recv)
-        split_data.reverse()
+    data = socket.receive_fixed_length(4)
+    assert data_to_recv == data
 
-        self.sock.recv = lambda x: split_data.pop()
+def test_receive_failed_response(socket):
+    responses = ['', 'hi']
+    socket.socket.recv = lambda x: responses.pop()
 
-        data =  self.mysock.receive_fixed_length(4)
-        self.assertEqual(data_to_recv, data)
-
-    def test_receive_failed_response(self):
-        responses = ['', 'hi']
-        self.sock.recv = lambda x: responses.pop()
-
-        with self.assertRaises(RuntimeError):
-            self.mysock.receive_fixed_length(100)
+    with pytest.raises(RuntimeError):
+        socket.receive_fixed_length(100)
