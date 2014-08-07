1920d60492171c40e9e64c285710fbfd2dade096
Noah Goldman <noahg34@gmail.com>
2014-08-07 15:32:00 -0400

    Bumped version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ class Tox(TestCommand):
 
 setup(
     name = "adbpy",
-    version = "0.1.1",
+    version = "0.1.2",
     url = "https://github.com/noahgoldman/adbpy",
     license = "MIT",
     author = "Noah Goldman",
